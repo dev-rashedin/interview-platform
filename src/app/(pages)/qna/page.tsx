94d378a9accd316f3451d86496a7663093d2db81
Rashedin-063 <rashedinislam.06@gmail.com>
2025-12-26 13:47:49 +0600

    mdx component added

--- src/app/(pages)/qna/page.tsx
+++ src/app/(pages)/qna/page.tsx
@@ -1,6 +1,8 @@
 const QnaPage = () => {
   return (
-    <div>page</div>
+    <main className="h-screen flex-center">
+      <h1>Interview Question Bank</h1>
+    </main>
   )
 }
 export default QnaPage
